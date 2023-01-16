1d8f30b3c57b5de391a42b3667b0127bf4cef379
Titus Wormer <tituswormer@gmail.com>
2023-01-16 20:05:50 +0100

    Add more JSDocs

--- index.js
+++ index.js
@@ -2,14 +2,21 @@
  * @typedef {import('xast').Root} Root
  * @typedef {import('xast').Element} Element
  * @typedef {import('xast').RootChildMap} RootChildMap
- * @typedef {RootChildMap[keyof RootChildMap]} Child
- * @typedef {Root|Child} Node
- * @typedef {Root|Element} Parent
  */
 
 /**
+ * @typedef {RootChildMap[keyof RootChildMap]} Child
+ * @typedef {Root | Child} Node
+ * @typedef {Root | Element} Parent
+ */
+
+/**
+ * Get the plain-text value of a node.
+ *
  * @param {Node} node
+ *   Node to serialize.
  * @returns {string}
+ *   Serialized node.
  */
 export function toString(node) {
   // A root or an element
@@ -18,8 +25,12 @@ export function toString(node) {
 }
 
 /**
+ * Serialize a child.
+ *
  * @param {Node} node
+ *   Child to serialize.
  * @returns {string}
+ *   Serialized node.
  */
 function one(node) {
   if (node.type === 'text') return node.value
@@ -28,8 +39,12 @@ function one(node) {
 }
 
 /**
+ * Serialize a parent.
+ *
  * @param {Parent} node
+ *   Parent to serialize.
  * @returns {string}
+ *   Serialized node.
  */
 function all(node) {
   const children = node.children
