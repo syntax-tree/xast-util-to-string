a4a72c1b7cb508833ef779b79366801cb0b4595a
Titus Wormer <tituswormer@gmail.com>
2023-07-18 12:44:57 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -2,63 +2,72 @@ import assert from 'node:assert/strict'
 import test from 'node:test'
 import {u} from 'unist-builder'
 import {toString} from './index.js'
-import * as mod from './index.js'
 
-test('toString', () => {
-  assert.deepEqual(
-    Object.keys(mod).sort(),
-    ['toString'],
-    'should expose the public api'
+test('toString', async function (t) {
+  await t.test('should expose the public api', async function () {
+    assert.deepEqual(Object.keys(await import('./index.js')).sort(), [
+      'toString'
+    ])
+  })
+
+  await t.test('should serialize cdata', async function () {
+    assert.deepEqual(
+      toString(u('cdata', '<greeting>Hello, world!</greeting>')),
+      '<greeting>Hello, world!</greeting>'
+    )
+  })
+
+  await t.test('should serialize comments', async function () {
+    assert.deepEqual(toString(u('comment', 'foo')), 'foo')
+  })
+
+  await t.test('should serialize instructions', async function () {
+    assert.deepEqual(
+      toString(
+        u('instruction', {name: 'xml'}, 'version="1.0" encoding="UTF-8"')
+      ),
+      'version="1.0" encoding="UTF-8"'
+    )
+  })
+
+  await t.test('should serialize texts', async function () {
+    assert.deepEqual(toString(u('text', 'foo')), 'foo')
+  })
+
+  await t.test('should return empty for doctypes', async function () {
+    assert.deepEqual(toString(u('doctype', {name: 'html'})), '')
+  })
+
+  await t.test(
+    'should serialize elements (excluding non-parent and non-text descendants)',
+    async function () {
+      assert.deepEqual(
+        toString(
+          u('element', {name: 'package', attributes: {}}, [
+            u('text', 'foo '),
+            u('comment', 'bar'),
+            u('element', {name: 'thing', attributes: {}}, [u('text', ' baz')])
+          ])
+        ),
+        'foo  baz'
+      )
+    }
   )
 
-  assert.deepEqual(
-    toString(u('cdata', '<greeting>Hello, world!</greeting>')),
-    '<greeting>Hello, world!</greeting>',
-    'should serialize cdata'
-  )
-
-  assert.deepEqual(
-    toString(u('comment', 'foo')),
-    'foo',
-    'should serialize comments'
-  )
-
-  assert.deepEqual(
-    toString(u('instruction', {name: 'xml'}, 'version="1.0" encoding="UTF-8"')),
-    'version="1.0" encoding="UTF-8"',
-    'should serialize instructions'
-  )
-
-  assert.deepEqual(toString(u('text', 'foo')), 'foo', 'should serialize texts')
-
-  assert.deepEqual(
-    toString(u('doctype', {name: 'html'})),
-    '',
-    'should return empty for doctypes'
-  )
-
-  assert.deepEqual(
-    toString(
-      u('element', {name: 'package', attributes: {}}, [
-        u('text', 'foo '),
-        u('comment', 'bar'),
-        u('element', {name: 'thing', attributes: {}}, [u('text', ' baz')])
-      ])
-    ),
-    'foo  baz',
-    'should serialize elements (excluding non-parent and non-text descendants)'
-  )
-
-  assert.deepEqual(
-    toString(
-      u('root', [
-        u('doctype', {name: 'html'}),
-        u('text', 'foo '),
-        u('comment', 'bar'),
-        u('element', {name: 'thing', attributes: {}}, [u('text', ' baz')])
-      ])
-    ),
-    'foo  baz',
-    'should serialize roots (excluding non-parent and non-text descendants)'
+  await t.test(
+    'should serialize roots (excluding non-parent and non-text descendants)',
+    async function () {
+      assert.deepEqual(
+        toString(
+          u('root', [
+            u('doctype', {name: 'html'}),
+            u('text', 'foo '),
+            u('comment', 'bar'),
+            u('element', {name: 'thing', attributes: {}}, [u('text', ' baz')])
+          ])
+        ),
+        'foo  baz'
+      )
+    }
   )
 })
